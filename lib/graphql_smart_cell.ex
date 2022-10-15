3ee294965d82f7af3db0be36b7ee814c4e7af07b
Michael Crumm <michael.crumm@cargosense.com>
2022-10-14 17:31:54 -0700

    Initial mix.new

---
+++ lib/graphql_smart_cell.ex
@@ -0,0 +1,18 @@
+defmodule GraphqlSmartCell do
+  @moduledoc """
+  Documentation for `GraphqlSmartCell`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GraphqlSmartCell.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/graphql_smart_cell_test.exs
@@ -0,0 +1,8 @@
+defmodule GraphqlSmartCellTest do
+  use ExUnit.Case
+  doctest GraphqlSmartCell
+
+  test "greets the world" do
+    assert GraphqlSmartCell.hello() == :world
+  end
+end

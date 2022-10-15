4b6a0a9c337e694c5d0e74caef2adab618ab9dd7
Michael Crumm <michael.crumm@cargosense.com>
2022-10-14 18:03:02 -0700

    mix deps and docs

--- mix.exs
+++ mix.exs
@@ -1,17 +1,23 @@
 defmodule GraphqlSmartCell.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "GraphQL integration with Livebook"
+
   def project do
     [
       app: :graphql_smart_cell,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: @version,
+      description: @description,
+      name: "GraphQL SmartCell",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +25,27 @@ defmodule GraphqlSmartCell.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:absinthe_client, "~> 0.1.0"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "components",
+      source_url: "https://github.com/spawnfest/graphql_smart_cell",
+      source_ref: "v#{@version}",
+      extras: ["guides/components.livemd"]
+    ]
+  end
+
+  def package do
+    [
+      links: %{
+        "GitHub" => "https://github.com/spawnfest/graphql_smart_cell"
+      }
     ]
   end
 end

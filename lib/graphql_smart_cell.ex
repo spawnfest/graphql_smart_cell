c7fca04f59ec60b668474ed259883f8fb7ed82ac
Jason Goldberger <jasongoldberger@gmail.com>
2022-10-15 12:04:23 -0500

    revert GraphqlSmartCell

--- lib/graphql_smart_cell.ex
+++ lib/graphql_smart_cell.ex
@@ -1,5 +1,18 @@
 defmodule GraphqlSmartCell do
   @moduledoc """
-  Stuff
+  Documentation for `GraphqlSmartCell`.
   """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GraphqlSmartCell.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
 end

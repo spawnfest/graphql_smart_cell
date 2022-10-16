31a86df52f6ba91e22cb072a3f84fb591a420c6a
Michael Crumm <michael.crumm@cargosense.com>
2022-10-16 16:29:05 -0700

    Remove EditorCell registration

--- lib/graphql_smart_cell/application.ex
+++ lib/graphql_smart_cell/application.ex
@@ -9,7 +9,6 @@ defmodule GraphqlSmartCell.Application do
   def start(_type, _args) do
     Kino.SmartCell.register(GraphqlSmartCell.ClientCell)
     Kino.SmartCell.register(GraphqlSmartCell.QueryCell)
-    Kino.SmartCell.register(GraphqlSmartCell.EditorCell)
 
     children = []
     opts = [strategy: :one_for_one, name: GraphqlSmartCell.Supervisor]
